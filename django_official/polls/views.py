3f80d8c67a83b6506b50d9846ef213f896c4e0f0
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-11-17 19:52:50 +0200

    finish polls app

--- django_official/polls/views.py
+++ django_official/polls/views.py
@@ -7,31 +7,40 @@ from django.core.urlresolvers import reverse
 # Create your views here.
 def index(request):
     questions = Question.objects.order_by('-pub_date')[:5]
-    # questions = Question.objects.all()
-    template = 'index.html'
+    template = 'polls/index.html'
     context = {'questions': questions}
     return render(request, template, context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question})
+    template = 'polls/detail.html'
+    context = {'question': question}
+    return render(request, template, context)
 
 
 def results(request, question_id):
-    return HttpResponse('Your looking at results of ' + question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    template = 'polls/results.html'
+    context = {'question': question}
+    return render(request, template, context)
 
 
 def vote(request, question_id):
-    p = get_object_or_404(Question, pk=question_id)
+    q = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+        selected_choice = q.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # redisplay the question form
-        return render(request, 'detail.html',
-                      {'question': p,
-                       'error_message': 'You didnt select a choice'})
+        template = 'polls/detail.html'
+        context = {
+            'question': q,
+            'error_message': 'You didnt select a choice'
+        }
+        return render(request,
+                        template,
+                        context)
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,)))
