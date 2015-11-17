22b9676bd356bfe4f0adea11f0d1b942459f473a
NikolaPavlov <nikola.pavlov1955@gmail.com>
2015-11-17 17:25:03 +0200

    polls until forms

--- django_official/mysite/urls.py
+++ django_official/mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    # include doesn't have $ at the end of regex
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
