1c74ecf0c88f3e9a56efb4ae4e29a08e0d105065
NikolaPavlov <nikola.pavlov1955@gmail.com>
2016-09-27 20:35:13 +0300

    added django official tutorial first commit

---
+++ django_official_tutoral/mysite/polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import Question
+
+# Create your views here.
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:3]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
