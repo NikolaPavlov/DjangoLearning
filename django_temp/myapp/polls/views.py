05a396db8026c24f8d0b572b00d2d65ba152046f
NikolaPavlov <nikola.pavlov1955@gmail.com>
2016-11-23 19:49:57 +0200

    switch django temp to django microsoft tutorial

--- django_temp/myapp/polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Question
-
-
-# Create your views here.
-def index(request):
-    questions = Question.objects.all()[:5]
-    context = {'questions': questions}
-    return render(request, 'polls/index.html', context)
